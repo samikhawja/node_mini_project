4eb3ece0634964c529394fa3e3bc723b376fa038
samikhawja <skhawja11@gmail.com>
2021-10-13 11:15:53 -0700

    template literal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,49 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
 
-const questions = [
+inquirer
+  .prompt([
     {
-        type: "input",
-        message: "?",
-        name: "",
+      type: 'input',
+      message: 'Name?',
+      name: 'name',
     },
     {
-        type: "input",
-        message: "?",
-        name: "",
+      type: 'input',
+      message: 'Age?',
+      name: 'age',
     },
     {
-        type: "input",
-        message: "?",
-        name: "",
+      type: 'input',
+      message: 'Height?',
+      name: 'height',
     },
-    fs.writeToFile('questions.txt', questions, error => {
-        error ? console.log(error) : "File Created"
-    })
-];
+  ])
+  .then((response) => {
+      console.log(response);
+  })
+
+    const generateHTML = (response) => {
+        `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Node Mini Project</title>
+        </head>
+        <body>
+            <h1>${response.name}</h1>
+            <h1>${response.age}</h1>
+            <h1>${response.height}</h1>
+            <script src="/assets/js/index.js"></script>
+        </body>
+        </html>
+        `
+    }
+
+
+  fs.writeToFile('index.html', generateHTML(response), error => {
+    error ? console.log(error) : "File Created"
+})
